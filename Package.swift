4c1c22c95479c19bf244716e0afe413b84747291
szabolcsadam-seon <szabolcs.adam@seon.io>
2024-12-06 15:33:04 +0100

    Update checksum for 5.2.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "5.4.2"
+let version = "5.2.0"
 
 let package = Package(
     name: "SeonSDK",
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "SeonSDK",
             url: "https://cdn.seon.io/sdk/ios/SeonSDK/\(version)/SeonSDK-\(version)-xcfw.zip",
-            checksum: "554756711bf17df0ce5ab7d50ad0eaffe831c5accd8dbe01078b6e9ebe85c21f"
+            checksum: "4ce00b25596ad96cadbc6eaba67ffe4b67a619e33b2b88c9c354ecb636fa21ef"
         )
     ]
 )
