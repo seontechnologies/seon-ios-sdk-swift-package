35950e88965623203ce59a1fbfd5eeb411c4d523
Robert Orsulics <robert.orsulics@seon.io>
2025-04-02 17:07:48 +0200

    [UPDATE] 5.5.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "5.5.2"
+let version = "5.5.3"
 
 let package = Package(
     name: "SeonSDK",
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "SeonSDK",
             url: "https://cdn.seon.io/sdk/ios/SeonSDK/\(version)/SeonSDK-\(version)-xcfw.zip",
-            checksum: "28467c4be6c3a6e3077ab38a6c1b2fcf6f465822dd9b1ef120b0227fcd2bc3ae"
+            checksum: "ac95e2ab2e668356313c22dc7edc87b4c5fa5fc952b6afa58c32f5368545cabd"
         )
     ]
 )
